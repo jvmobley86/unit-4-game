94b8b750a6b9484e481249857b3f1c0f6526adde
Teiti <jessicavmobley@gmail.com>
2019-06-13 21:06:12 -0500

    added some functionality.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,13 +8,14 @@
         var losses = 0;
         var wins = 0;
         var totalScore = 0;
-
+     
+    var gameReset = function() {
     $("#computerPick").text(computerPick); 
     
 
-        for (var i=0; i < 4; i++){
-    var random = Math.floor(Math.random() * 11)+1;
-    console.log(random);
+    for (var i=0; i < 4; i++){
+        var random = Math.floor(Math.random() * 11)+1;
+        console.log(random);
 
 //Random number for each crystal
 
@@ -23,34 +24,42 @@
         "randomNumber":random
     })
     $(".crystals").append(crystals);
+}  
 }
-//New random number assigned after a win or loss
- if (wins++ || losses++ ==true, 
- $("#computerPick").attr("randomNumber")
-    );
 
+
+
+//New random number assigned after a win or loss
+//  if (wins++ || losses++ ==true, 
+//  $("#computerPick").attr("randomNumber")
+//     );
+    gameReset ();
 //Add clicked crystal value to total score
     $(".crystals").on("click", function(){
        totalScore += Number($(this).attr("randomNumber")) 
-    
-    if (totalScore === computerPick)
-        wins++
-       $("#wins").text(wins)
-        
-    if (totalScore > computerPick)
-       losses++
+//until total score is equal to the computer pick    
+    if (totalScore === computerPick) {
+        wins++;
+       $("#wins").text(wins) 
+ }
+{
+
+//Game resets if total score is greater than computer pick and add 1 to losses counter    
+
+if (totalScore > computerPick)
+       losses++;
       $("#losses").text(losses)
-    
+      gameReset ();
+}      
+
 
        $("#totalScore").text(totalScore);
-    
-       $("#wins").text(wins);
-       $("#losses").text(losses);
+       $("#wins").text(wins)
+       $("#losses").text(losses)
     });
 
-//until total score is equal to the computer pick
 
-//Game resets if total score is greater than computer pick and add 1 to losses counter
+
 
 //If total score equals computer pick, add 1 to wins counter
 
